693a274dc4c188670470852fe072a5fc5a4a741a
lifankeke <lifankeke@foxmail.com>
2020-06-20 20:29:39 +0800

    练习

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 const koa = require('koa')
 const app = new koa();
-const bodyParser = require ( 'koa-bodyparser ' )
+const bodyParser = require ('koa-bodyparser')
 
 app.listen(3001, () => {
     console.log('serve is running in port 3001')
 })
 
-// app.use(async (ctx, next) => {
-//     await next();
-//     ctx.response.type = 'text/html';
-//     ctx.response.body = '<h1>hello world</h1>'
-// })
-
-let postData = ctx.request.body
+app.use(async (ctx, next) => {
+    await next();
+    ctx.response.type = 'text/html';
+    ctx.response.body = '<h1>hello world</h1>'
+})
