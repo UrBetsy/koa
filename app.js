a0dd5b73ce94c7106889c556c120b13a94358924
lifankeke <lifankeke@foxmail.com>
2020-07-02 15:44:26 +0800

    get请求数据获取

--- app.js
+++ app.js
@@ -3,6 +3,28 @@ const app = new koa();
 const router = require('./middleware/router')
 
 app.use(router.routes())
+.use( async(ctx) => {
+    let url = ctx.request.url;
+    /**
+     * 从上下文的request对象中获取请求相关信息
+     */
+    let request = ctx.request;
+    let req_query = request.query;
+    let req_querystring = request.querystring;
+    /**
+     * 从上下文中直接获取
+     */
+    let ctx_query = ctx.query;
+    let ctx_querystring = ctx.querystring;
+    ctx.body = {
+        url,
+        req_query,
+        req_querystring,
+        ctx_query,
+        ctx_querystring
+    }
+
+})
 app.listen(3001, () => {
     console.log('serve is running in port 3001')
 })
--- controller/home.js
+++ controller/home.js
@@ -1,22 +1,25 @@
-const Home = async (ctx) => {
+const Home = async (ctx, next) => {
     let html = `
         我是index
     `
     ctx.body = html;
+    next()
 }
 
-const Todo = async (ctx) => {
+const Todo = async (ctx, next) => {
     let html = `
         我是todo
     `
     ctx.body = html;
+    next()
 }
 
-const NotFound = async (ctx) => {
+const NotFound = async (ctx, next) => {
     let html = `
         我是NotFound
     `
     ctx.body = html;
+    next()
 }
 
 module.exports = {
