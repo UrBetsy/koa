b194d6b9c698fb2ecd6b97f21d73f84b3f7b3624
lifankeke <lifankeke@foxmail.com>
2020-07-02 17:42:46 +0800

    cookie

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const koa = require('koa')
 const path = require('path')
-const static = require('koa-static')
 
 const app = new koa();
 
-const staticPath = './static'
-
-app.use(static(path.join(__dirname, staticPath)))
+app.use(async (ctx) => {
+    if(ctx.url == '/index') {
+        ctx.cookies.set(
+            'cid', 
+            'hello world',
+            {
+              domain: 'localhost',  // 写cookie所在的域名
+              path: '/index',       // 写cookie所在的路径
+              maxAge: 10 * 60 * 1000, // cookie有效时长
+              expires: new Date('2017-02-15'),  // cookie失效时间
+              httpOnly: true,   // 是否只用于http请求中获取
+              overwrite: false  // 是否允许重写
+            }
+          )
+        ctx.body = 'cookie is ok'
+    } else {
+        ctx.body = 'hello world'
+    }
+})
 
 app.listen(3001, () => {
     console.log('serve is running in port 3001')
