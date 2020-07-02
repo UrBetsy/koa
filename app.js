3d7cbb3abd9cd3acea3899d316847aa9f6d9676c
lifankeke <lifankeke@foxmail.com>
2020-07-02 17:19:49 +0800

    原生静态资源处理

--- app.js
+++ app.js
@@ -1,33 +1,44 @@
 const koa = require('koa')
+const path = require('path')
 const app = new koa();
-const router = require('./middleware/router')
-const bodyParser = require('koa-bodyparser')
+const content = require('./util/content')
+const mimes = require('./util/mimes')
 
-app.use(bodyParser())
-   .use( async (ctx) => {
-    if (ctx.url === '/' && ctx.method == 'GET') {
-        // 当get请求的时候先返回表单
-        let html = `
-        <h1>koa2 request post demo</h1>
-        <form method="POST" action="/">
-            <p>userName</p>
-            <input name="userName" /><br/>
-            <p>nickName</p>
-            <input name="nickName" /><br/>
-            <p>email</p>
-            <input name="email" /><br/>
-            <button type="submit">submit</button>
-        </form>
-        `
-        ctx.body = html
-    } else if (ctx.url == '/' && ctx.method === 'POST') {
-        // 当post请求的时候
-        let postData = ctx.request.body
-        ctx.body = postData
+const staticPath = './static'
+
+// 解析静态资源
+function parseMine( url ) {
+    let extName = path.extname(url);
+    extName = extName ? extName.slice(1) : 'unknow'
+    return mimes[ extName ]
+}
+
+app.use (async (ctx)=> {
+    let fullStaticPath = path.join(__dirname, staticPath)
+
+    let _content = await content(ctx, fullStaticPath)
+
+    let _mime = parseMine( ctx.url )
+
+    if (_mime) {
+        ctx.type = _mime
+    }
+
+    // 输出静态资源内容
+    if ( _mime && _mime.indexOf('image/') >= 0 ) {
+        // 如果是图片，则用node原生res，输出二进制数据
+        ctx.res.writeHead(200)
+        ctx.res.write(_content, 'binary')
+        ctx.res.end()
     } else {
-        ctx.body = '<h1>404！！！ o(╯□╰)o</h1>'
+        // 其他则输出文本
+        ctx.body = _content
     }
 })
+
+
+
+
 
 app.listen(3001, () => {
     console.log('serve is running in port 3001')
