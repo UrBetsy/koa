caa1e93739d263cbb23bcdb42b2f5dfbc2507809
lifankeke <lifankeke@foxmail.com>
2020-07-02 16:09:32 +0800

    使用parsebody中间件

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const koa = require('koa')
 const app = new koa();
 const router = require('./middleware/router')
+const bodyParser = require('koa-bodyparser')
 
-app.use( async (ctx) => {
+app.use(bodyParser())
+   .use( async (ctx) => {
     if (ctx.url === '/' && ctx.method == 'GET') {
         // 当get请求的时候先返回表单
         let html = `
@@ -20,39 +22,13 @@ app.use( async (ctx) => {
         ctx.body = html
     } else if (ctx.url == '/' && ctx.method === 'POST') {
         // 当post请求的时候
-        let postData = await parsePostData( ctx )
+        let postData = ctx.request.body
         ctx.body = postData
     } else {
         ctx.body = '<h1>404！！！ o(╯□╰)o</h1>'
     }
 })
-function parsePostData( ctx ) {
-    return new Promise((resolve, reject) => {
-      try {
-        let postdata = "";
-        ctx.req.addListener('data', (data) => {
-          postdata += data
-        })
-        ctx.req.addListener("end",function(){
-          let parseData = parseQueryStr( postdata )
-          resolve( parseData )
-        })
-      } catch ( err ) {
-        reject(err)
-      }
-    })
-}
-// 将POST请求参数字符串解析成JSON
-function parseQueryStr( queryStr ) {
-    let queryData = {}
-    let queryStrList = queryStr.split('&')
-    console.log( queryStrList )
-    for (  let [ index, queryStr ] of queryStrList.entries()  ) {
-      let itemList = queryStr.split('=')
-      queryData[ itemList[0] ] = decodeURIComponent(itemList[1])
-    }
-    return queryData
-}
+
 app.listen(3001, () => {
     console.log('serve is running in port 3001')
 })
